aec609d22896b205424c1a06806c8f1938230877
Dima Nikolaenko <dima.nik.dev@gmail.com>
2020-06-28 09:34:37 +0300

    Initial commit

---
+++ CitiesTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitiesTestProject
+//
+//  Created by Dima Nikolaenko on 28.06.2020.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
