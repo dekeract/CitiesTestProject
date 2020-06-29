a6ac7dbde2a62b77312a2922682e554c11ae6186
Dima Nikolaenko <dima.nik.dev@gmail.com>
2020-06-29 16:38:19 +0300

    Use binary search as an array filtering algorithm

--- CitiesTestProject/Helpers/CitySearch.swift
+++ CitiesTestProject/Helpers/CitySearch.swift
@@ -14,6 +14,37 @@ protocol CitySearching {
 
 class CitySearch: CitySearching {
     func filter(cities: [City], with searchedText: String) -> [City] {
-        return cities.filter { $0.searchName.lowercased().starts(with: searchedText.lowercased()) }
+        let lowerBoundIndex = firstOccurrenceIndex(of: searchedText, from: 0, to: cities.count - 1, in: cities)
+        guard lowerBoundIndex >= 0 else { return [] }
+        let upperBoundIndex = lastOccurrenceIndex(of: searchedText, from: lowerBoundIndex, to: cities.count, in: cities)
+        guard upperBoundIndex >= lowerBoundIndex, upperBoundIndex < cities.count else { return [] }
+        
+        return Array(cities[lowerBoundIndex...upperBoundIndex])
+    }
+    
+    private func firstOccurrenceIndex(of searchText: String, from lowerBound: Int, to upperBound: Int, in cities: [City]) -> Int {
+        guard lowerBound <= upperBound else { return -1 }
+        
+        let midIndex = (lowerBound + upperBound) / 2
+        if cities[midIndex].searchName.lowercased().starts(with: searchText.lowercased()) && (midIndex == 0 || searchText.lowercased() > cities[midIndex - 1].searchName.lowercased()) {
+            return midIndex
+        } else if searchText.lowercased() > cities[midIndex].searchName.lowercased() {
+            return firstOccurrenceIndex(of: searchText, from: midIndex + 1, to: upperBound, in: cities)
+        } else {
+            return firstOccurrenceIndex(of: searchText, from: lowerBound, to: midIndex - 1, in: cities)
+        }
+    }
+    
+    private func lastOccurrenceIndex(of searchText: String, from lowerBound: Int, to upperBound: Int, in cities: [City]) -> Int {
+        guard lowerBound <= upperBound else { return -1 }
+        
+        let midIndex = (lowerBound + upperBound) / 2
+        if cities[midIndex].searchName.lowercased().starts(with: searchText.lowercased()) && (midIndex == cities.count - 1 || searchText.lowercased() < cities[midIndex + 1].searchName.lowercased()) {
+            return midIndex
+        } else if searchText.lowercased() < cities[midIndex].searchName.lowercased() {
+            return lastOccurrenceIndex(of: searchText, from: lowerBound, to: midIndex - 1, in: cities)
+        } else {
+            return lastOccurrenceIndex(of: searchText, from: midIndex + 1, to: upperBound, in: cities)
+        }
     }
 }
